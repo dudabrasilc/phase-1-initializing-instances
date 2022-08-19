9e0e203df5f9a801418953bbd49abe9d2cb802e9
Maria Coitinho <brasildu@gmail.com>
2022-08-19 18:39:05 -0500

    commit tree 16e9d57e5e903a21bb118cfdba6766788dedc546

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+const breakfast = new Breakfast("doritos", "water")
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  #desert;
+  constructor(salad, soup, entree, desert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#desert = desert;
+  }
+}
